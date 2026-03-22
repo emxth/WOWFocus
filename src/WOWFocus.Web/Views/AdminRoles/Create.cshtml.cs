76a4bc6db59aaba736be7fca771b1e7f9e806770
emxth <randivemith.2003@gmail.com>
2026-03-22 16:17:09 +0530

    Add admin roles view for create view and update

---
+++ src/WOWFocus.Web/Views/AdminRoles/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WOWFocus.Web.Views.AdminRoles
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
