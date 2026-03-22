c737cc8168d8312f7e83f14db4bba14ee4c6881a
emxth <randivemith.2003@gmail.com>
2026-03-22 16:19:22 +0530

    Modify for user authentication and rolle bassed access

--- src/WOWFocus.Web/Program.cs
+++ src/WOWFocus.Web/Program.cs
@@ -1,22 +1,39 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using WOWFocus.Application.Interfaces;
 using WOWFocus.Application.Services;
 using WOWFocus.Infrastructure.Repositories;
-using WOWFocus.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-var studentsFile = builder.Configuration["JsonStorage:StudentsFile"]!;
-builder.Services.AddScoped<IStudentRepository>(_ => new JsonStudentRepository(studentsFile));
-builder.Services.AddScoped<IStudentService, StudentService>();
+var identityFile = builder.Configuration["JsonStorage:IdentityFile"]!;
+builder.Services.AddScoped<IIdentityRepository>(_ => new JsonIdentityRepository(identityFile));
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<IdentityAdminService>();
 
-builder.Services.AddHostedService<ArchivedStudentCleanupService>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(opt =>
+    {
+        var hours = builder.Configuration.GetValue<int>("Auth:SessionHours");
+        opt.LoginPath = "/Account/Login";
+        opt.ExpireTimeSpan = TimeSpan.FromHours(hours);
+        opt.SlidingExpiration = true;
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("perm:admin.manage",
+        policy => policy.RequireClaim("perm", "admin.manage"));
+});
 
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapDefaultControllerRoute();
 app.Run();
