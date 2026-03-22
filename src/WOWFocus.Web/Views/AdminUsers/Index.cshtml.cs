4e81a474452b3073dedcbcc12dd37ef41f37a483
emxth <randivemith.2003@gmail.com>
2026-03-22 16:17:55 +0530

    Add admin create, view, edit views

---
+++ src/WOWFocus.Web/Views/AdminUsers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WOWFocus.Web.Views.AdminUsers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
